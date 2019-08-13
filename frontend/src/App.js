d1bfc6d2d1fb73501dc14ee7a1f551a2e407e7ad
Robert Flanagan <robertflanagan95@gmail.com>
2019-08-12 22:37:43 -0400

    moving files to new repo

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Main from "./components/Main/main";
+
+function App() {
+  return (
+    <div>
+      <Main />
+      <div className="footer">
+        <a href="https://github.com/rjflanagan95/keto-tracker">View the code on GitHub</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
